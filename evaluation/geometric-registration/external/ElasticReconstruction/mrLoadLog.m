19ea5aea8951aab2a3e2a4fc19afba4e39a3a348
gabriel-corteletti <gabriellcorteletti@gmail.com>
2024-07-04 17:19:33 +0200

    first commit

--- evaluation/geometric-registration/external/ElasticReconstruction/mrLoadLog.m
+++ evaluation/geometric-registration/external/ElasticReconstruction/mrLoadLog.m
@@ -1,5 +1,19 @@
-function [ traj ] = mrLoadLog( filename )
-    fid = fopen( filename );
+function [ traj, success ] = mrLoadLog( filename )
+    
+    % Initialize traj as an empty struct array
+    traj = struct('info', {}, 'trans', {});
+    success = false; % Flag to indicate if reading was successful
+
+    % Attempt to open the file
+    fid = fopen( filename, 'r' );
+
+    % Check if the file was successfully opened
+    if fid == -1
+        disp(['Error opening file: ', filename]);
+        return;
+    end
+
+    % Read the data
     k = 1;
     x = fscanf( fid, '%d', [1 3] );
     while ( size( x, 2 ) == 3 )
@@ -10,4 +24,12 @@ function [ traj ] = mrLoadLog( filename )
     end
     fclose( fid );
     %disp( [ num2str( size( traj, 2 ) ), ' frames have been read.' ] );
+
+    % Check if any frames were read
+    if isempty(traj)
+        return;
+    end
+    
+    % If no problem was found
+    success = true; % Indicate successful reading
 end
